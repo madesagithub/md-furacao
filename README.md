b27b8a2ef2767125486e99bc6a26827c0633deab
Tiago Lucas Flach <tiagolucas9830@gmail.com>
2022-04-18 19:08:19 -0300

    Primeira distribuição de furos

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # MD-Furação
 
+
 ## Roadmap
 - [ ] Automatizar a informação das informações de furação.
 
 - [ ] Criar mecanismo automático que crie padrão de furação para as furadeiras lidear através da interação dos softwares Ardis e Topsolid.
 
 - [ ] Criar interface gráfica para consulta das informações na máquina.
+
+
+## Lógica de programação
+
+1. Agrupar os furos por `side`;
+2. Novo agrupamento:
+	- Caso `side` seja igual a `0:1` ou `0:3`:
+		- Agrupar subgrupos por `Y`;
+	- Caso contrário:
+		- Agrupar subgrupos por `X`;
+3. Varrer a lista de furos agrupados pelo `side`;
+4. Varrer a lista de cabeçotes até encontrar o primeiro que seja da posição desejada e que não esteja sendo utilizado.
+5. Varrer a lista de furo agrupados por `side` e (`x` ou `y`) e definir em qual broca o furo será colocado.
+
 
 ## Definições
 
@@ -15,9 +30,9 @@
 | **CRN**			 	| Corresponde ao lado de furação		   	|
 | **DP**			   	| Profundidade do furo					 	|
 | **TRH ou T**		 	| Se é um furo passante (atravessa a peça) 	|
-| **DIA ou Diâmetro**   | Diâmetro do furo						 	|
+| **DIA ou Diâmetro**   | Diâmetro do furo							|
 | **X**					| Eixo horizontal (esquerda para direita)  	|
-| **Y**					| Eixo vertical	(cima para baixo)           |
+| **Y**					| Eixo ~vertical (cima para baixo)~	?distancia? (trás para frente)		|
 | **Z**					|										 	|
 
 
@@ -35,6 +50,9 @@
 - Posições
 	- Primeiro cabeçote: `0:1`
 	- Último cabeçote: `0:3`
+
+
+### Furadeira
 
 | Cabeçote  | Posição 	    | Ordenamento 	| Observação 					|
 |---------- |-------------- |-------------- |------------------------------ |
@@ -57,10 +75,3 @@
 | 17		| Traseiro  	| 2		 		|								|
 | 18		| Traseiro	    | 3		 		|								|
 | 19		| Traseiro  	| 4		 		|								|
-
-## Lógica de programação
-### Casos
-- Caso `Side == 0:1` ou `Side == 0:3`
-	- Agrupar elementos onde `Y` e `Side` sejam iguais
-- Senão
-	- Agrupar elementos onde `X` e `Side` sejam iguais
--- furacao.py
+++ furacao.py
@@ -1,5 +1,5 @@
 # import pandas as pd
-from multiprocessing.pool import TERMINATE
+from typing import OrderedDict
 import numpy as np
 import pandas as pd
 
@@ -9,25 +9,48 @@ from collections import defaultdict
 from Cabecote import Cabecote
 from Furo import Furo
 
-# Variáveis de configuração
-nro_cabecotes = 16
-nro_brocas = 21
-distancia_pinos = 32 # mm
-
-posicao_cabecotes = {
-    'esquerda': [1],
-    'direita': [10],
-    'inferior': list(range(2, 10)),
-    'superior': list(range(11, 16)),
-    'traseiro': list(range(16, 20))
-}
-# ----------
-
 # Arquivo
 filename = 'DIVISÓRIA 12X387X1652.bpp'
 filename = 'TAMPO MAL 15X440X2280.bpp'
 file = open(filename, 'r', encoding='latin1')
 # ----------
+
+# Variáveis de configuração
+furadeira = 'furadeira-2'
+
+# Configuração de furadeiras
+furadeiras = {
+	'furadeira-1': {
+		'nro_cabecotes': 19,
+		'nro_brocas': 21,
+		'distancia_pinos': 32,
+		'posicao_cabecotes': {
+			'esquerda': [1],
+			'direita': [10],
+			'inferior': list(range(2, 10)),
+			'superior': list(range(11, 16)),
+			'traseiro': list(range(16, 20)),
+		},
+	},
+	'furadeira-2': {
+		'nro_cabecotes': 16,
+		'nro_brocas': 21,
+		'distancia_pinos': 32,
+		'posicao_cabecotes': {
+			'esquerda': [1],
+			'direita': [16],
+			'inferior': list(range(2, 10)),
+			'superior': list(range(10, 16)),
+		},
+	}
+}
+# ----------
+
+# Definição de variáveis
+nro_cabecotes = furadeiras[furadeira]['nro_cabecotes']
+posicao_cabecotes = furadeiras[furadeira]['posicao_cabecotes']
+nro_brocas = furadeiras[furadeira]['nro_brocas']
+distancia_pinos = furadeiras[furadeira]['distancia_pinos']
 
 # Definições
 # Side 0 : 0 - Horizontal inferior
@@ -44,11 +67,6 @@ for i in range(1, nro_cabecotes + 1):
 			cabecote = Cabecote(i, nro_brocas, distancia_pinos, j)
 			cabecotes.append(cabecote)
 			break
-	
-	# vetor = np.array([0 for i in range(nro_pinos)])
-	# vetor = np.array([0, 1, 2, 3])
-	# print(vetor)
-	# cabecotes[cabecote] = vetor
 
 # furos = {}
 # furos = np.array([])
@@ -91,7 +109,7 @@ for line in file:
 		line = line.split(',')
 
 		# Dados
-		side = line[5].removeprefix(' ')
+		side = line[5].strip()
 		crn = int(line[6].removeprefix(' ').replace('"', ''))
 		x = float(line[7].removeprefix(' '))
 		y = float(line[8].removeprefix(' '))
@@ -121,7 +139,9 @@ for line in file:
 
 # --------------------------------------------------
 
+
 # Tabela da Peça
+# --------------------
 peca = PrettyTable()
 peca.title = nome
 peca.field_names = ['Dimensão', 'Valor (mm)']
@@ -130,9 +150,11 @@ peca.add_row(['Comprimento (X)', lpx])
 peca.add_row(['Largura (Y)', lpy])
 peca.add_row(['Espessura (Z)', lpz])
 print(peca)
-# ----------
+# --------------------
+
 
 # Tabela de dados
+# --------------------
 data = PrettyTable()
 data.title = nome
 data.field_names = ["Side", "CRN", "X", "Y", "Z", "DP", "Diametro", "P", "Broca"]
@@ -142,23 +164,8 @@ for i in furos:
 	data.add_row(list(i.__dict__.values()))
 
 print(data)
-# ----------
+# --------------------
 
-# Tabela de cabeçotes
-table = PrettyTable()
-table.title = 'Cabeçotes'
-table.field_names = list(cabecote.nro for cabecote in cabecotes)
-
-# Brocas
-for i in range(1, nro_brocas + 1):
-	table.add_row(list(cabecote.brocas[i] for cabecote in cabecotes))
-
-# Distancia x
-table.add_row(list('-' for cabecote in cabecotes))
-table.add_row(list(cabecote.x for cabecote in cabecotes))
-table.add_row(list(cabecote.posicao[0] for cabecote in cabecotes))
-print(table)
-# ----------
 
 
 
@@ -209,6 +216,130 @@ print(data)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+# Agrupar por side
+groups = defaultdict(list)
+for furo in furos:
+	groups[furo.side].append(furo)
+furos = dict(groups.items())
+
+
+
+
+
+
+for side in furos:
+
+	# Define a posição
+	if side == '0 : 0':
+		posicao = 'inferior'
+	elif side == '0 : 1':
+		posicao = 'esquerda'
+	elif side == '0 : 2':
+		posicao = ''
+	elif side == '0 : 3':
+		posicao = 'direita'
+	elif side == '0 : 4':
+		posicao = ''
+	elif side == '0 : 5':
+		posicao = 'superior'
+
+
+	# Inferior
+	# Superior
+	# --------------------
+	if side == '0 : 0' or side == '0 : 5':
+		# Agrupar por x
+		groups_x = defaultdict(list)
+		for furo in furos[side]:
+			groups_x[furo.x].append(furo)
+		groups_x = dict(groups_x.items())
+
+		# Ordenar
+		groups_x = dict(OrderedDict(sorted(groups_x.items())))
+
+		# Provávelmente não haverá casos onde x tenha multiplos e nçao multiplos
+		# Agrupar por múltiplos de distancia_pinos
+		# groups_x_dp = {}
+		# for x in list(groups_x.keys()):
+		# 	groups_x_dp[x] = {'multiplos': [], 'não multiplos': []}
+		# 	for furo in groups_x[x]:
+		# 		if furo.y % distancia_pinos == 0:
+		# 			groups_x_dp[x]['multiplos'].append(furo)
+		# 		else:
+		# 			groups_x_dp[x]['não multiplos'].append(furo)
+
+		# Selecionar cabeçotes
+		for x in groups_x:
+
+			for cabecote in cabecotes:
+				if cabecote.posicao == posicao and cabecote.used == False:
+					break
+					
+			cabecote.use()
+			cabecote.setX(x)
+
+			# Aplica os furos
+			for furo in groups_x[x]:
+				cabecote.setBroca(furo)
+	
+
+	# Esquerda
+	# Direita
+	# --------------------
+	# Inverte x e y????
+	elif side == '0 : 1' or side == '0 : 3':
+		# Agrupar por y
+		groups_y = defaultdict(list)
+		for furo in furos[side]:
+			groups_y[furo.y].append(furo)
+		groups_y = dict(groups_y.items())
+
+		# Ordenar
+		groups_y = dict(OrderedDict(sorted(groups_y.items())))
+		
+		# Selecionar cabeçotes
+		for y in groups_y:
+
+			for cabecote in cabecotes:
+				if cabecote.posicao == posicao and cabecote.used == False:
+					break
+					
+			cabecote.use()
+			cabecote.setX(y)
+
+			# Aplica os furos
+			for furo in groups_y[y]:
+				cabecote.setBroca(furo, 'x')
+
+
+	# Traseira
+	# Frontal
+
+
+
+# Ordenar por X
+# for array in furos:
+# 	groups = defaultdict(list)
+# 	for obj in array:
+# 		groups[obj.x].append(obj)
+# 	array = list(groups.values())
+# 	arrays.append(array)
+
+
+
+
+
 # Agrupar por X
 # groups = defaultdict(list)
 # for obj in furos:
@@ -228,16 +359,31 @@ data.field_names = ["Side", "CRN", "X", "Y", "Z", "DP", "Diametro", "P", "Broca"
 # print(data)
 # furos.map(key=lambda furo: furo.x)
 
+# Tabela de cabeçotes
+# --------------------
+table = PrettyTable()
+table.title = 'Cabeçotes'
+table.field_names = list(cabecote.nro for cabecote in cabecotes)
 
-from operator import itemgetter
-from itertools import groupby
+# Brocas
+for i in range(1, nro_brocas + 1):
+	table.add_row(list(cabecote.brocas[i] for cabecote in cabecotes))
 
-lki = [["A",0], ["B",1], ["C",0], ["D",2], ["E",2]]
-lki.sort(key=itemgetter(1))
+# Distancia x
+table.add_row(list('-' for cabecote in cabecotes))
+table.add_row(list(cabecote.x for cabecote in cabecotes))
+table.add_row(list(cabecote.posicao[0:3] for cabecote in cabecotes))
 
-# print(lki)
-# print(furos)
-glo = [[x for x,y in g]
-	   for k,g in  groupby(lki,key=itemgetter(1))]
+# Índice
+indice = ''
+table._field_names.insert(0, indice)
+table._align[indice] = 'c' 
+table._valign[indice] = 't' 
+for i, _ in enumerate(table._rows): 
+	if i < nro_brocas: 
+		table._rows[i].insert(0, (i+1) * distancia_pinos) 
+	else:
+		table._rows[i].insert(0, '') 
 
-# print(glo)
+print(table)
+# --------------------
